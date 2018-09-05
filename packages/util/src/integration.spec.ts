fd1034757d281763230159af7829e4a5c717451a
Chris Casola <chriscasola@gmail.com>
2018-09-04 23:42:37 -0400

    test(exec): add integration test for exec

---
+++ packages/util/src/integration.spec.ts
@@ -0,0 +1,10 @@
+import { exec } from './exec';
+
+test('integration test for exec', async () => {
+  const cwd = __dirname;
+  const result = await exec(`echo "hello world"`, cwd);
+  expect(result).toMatchInlineSnapshot(`
+"hello world
+"
+`);
+});
