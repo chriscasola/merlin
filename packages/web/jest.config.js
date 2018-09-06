8531c67dadfb0411c8a549c2b2f6ecc6e6d791de
Chris Casola <chriscasola@gmail.com>
2018-09-05 22:36:58 -0400

    chore: add "@merl/web" package
    Add a new package to aid in development on
    web-related projects. It will help with
    node, npm, bower, etc. operations.

---
+++ packages/web/jest.config.js
@@ -0,0 +1,4 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+const baseConfig = require('../../jest.config.base');
+module.exports = baseConfig;
